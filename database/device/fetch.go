bbd1efe32423b296fd0ad04f6be5dec43993b8c0
cielaton <vuwin24680@gmail.com>
2024-11-24 21:14:48 +0700

    [Device] Added the option to query all the devices when no userId is specified.

--- database/device/fetch.go
+++ database/device/fetch.go
@@ -10,23 +10,45 @@ import (
 func GetDevicesInfo(database *pgx.Conn, userID string) ([]databaseModels.Device, error) {
 	// One user can have many devices
 	var devices []databaseModels.Device
+	var queriedRows pgx.Rows
+	var err error
 	tempDevice := databaseModels.Device{}
 
 	// Query the device(s) from database
-	rows, err := database.Query(context.Background(), "SELECT * FROM \"Device\" WHERE \"UserID\" = $1", userID)
-	if err != nil {
-		log.Err(err).Msg("[Database] Failed to get devices info")
-		return devices, err
+	// Query only for a user
+	if len(userID) > 0 {
+		queriedRows, err = database.Query(context.Background(), "SELECT * FROM \"Device\" WHERE \"UserID\" = $1", userID)
+		if err != nil {
+			log.Err(err).Msg("[Database] Failed to get devices info")
+			return devices, err
+		}
+		// Query for all devices
+	} else {
+		queriedRows, err = database.Query(context.Background(), "SELECT * FROM \"Device\"")
+
+		if err != nil {
+			log.Err(err).Msg("[Database] Failed to get all of the devices")
+			return devices, err
+		}
 	}
 
 	// Assign the returned database query value to the array of Device
-	for rows.Next() {
-		err := rows.Scan(&tempDevice.DeviceID, &tempDevice.UserID, &tempDevice.PrivateKey)
+	for queriedRows.Next() {
+		err := queriedRows.Scan(&tempDevice.DeviceID, &tempDevice.UserID, &tempDevice.PrivateKey)
 		if err != nil {
 			log.Err(err).Msg("[Database] Failed to scan device values.")
 			return devices, err
 		}
 
+		devices = append(devices, tempDevice)
+	}
+	// Assign the result to an array of Device(s)
+	for queriedRows.Next() {
+		err := queriedRows.Scan(&tempDevice.DeviceID, &tempDevice.UserID, &tempDevice.PrivateKey)
+		if err != nil {
+			log.Err(err).Msg("[Database] Failed to scan device values.")
+			return devices, err
+		}
 		devices = append(devices, tempDevice)
 	}
 
